cb20485948f81da99234d443d8a8ed06c038fb93
ChrisKnowsDev <chrisharley81@gmail.com>
2024-01-16 13:36:22 -0500

    changed styling of sidebar

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,3 +1,3 @@
 export default function Logo() {
-  return <h3 className="text-2xl font-bold">jobMate</h3>
+  return <h3 className="text-2xl font-bold pl-4">jobMate</h3>
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -11,16 +11,13 @@ export default function Sidebar() {
 
   return (
     <aside className="py-4 px-8 bg-muted h-full">
-      {/* Logo */}
       <Logo />
-
-      {/* links */}
       <div className="flex flex-col mt-20 gap-y-8">
         {links.map(link => (
           <Button
             asChild
             key={link.href}
-            variant={pathname === link.href ? 'outline ' : 'link'}
+            variant={pathname === link.href ? 'outline' : 'link'}
           >
             <Link href={link.href}>
               {link.icon} <span>{link.label}</span>
